70fcfd718c8c0dfc380d4d602bda6937522d4635
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2023-01-25 22:27:45 +0530

    Factorial of n function

--- Algorithms1.ts
+++ Algorithms1.ts
@@ -1,3 +1,4 @@
+// From Vishwas Video 7
 // Fibonacci Sequence
 const fibonacci = (n: number): number[] => {
   const fib = [0, 1]
@@ -6,5 +7,23 @@ const fibonacci = (n: number): number[] => {
   }
   return fib
 }
-
 console.log(fibonacci(7))
+
+// Factorial
+// From Vishwas Video 8
+const factorial = (n: number): number => {
+  let result = 1
+  for (let i = 2; i <= n; i++) {
+    result *= i
+  }
+  return result
+}
+console.log("factorial", factorial(5))
+// Big-O = O(n)
+// Mine
+const factorial2 = (n: number): number => {
+  if (n <= 0) {
+    return 1
+  }
+  return factorial2(n - 1) * n
+}
