c68e40e92c361412d66a4f31103ae6113f14e2bb
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2023-02-26 18:18:47 +0530

    Create v2-array.ts

---
+++ DataStructures/v2-array.ts
@@ -0,0 +1,9 @@
+// Array - Big-O time comlexity
+
+// Insert / remove from end - O(1)
+// Insert / remove from beginning - O(n)
+// Access - O(1)
+// Search - O(n)
+// Push / pop - O(1)
+// Shift / unshift / concat / slice / splice - O(n)
+// forEach / map / filter / reduce - O(n)
